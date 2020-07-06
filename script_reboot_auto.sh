bbb85b5054dc7393fa33104a4cc0e036c141a912
Arnaud Loisy <loisy.arnaud@gmail.com>
2020-07-06 21:49:07 +0200

    Update script_reboot_auto.sh
    wait 10s for logs to be written

--- script_reboot_auto.sh
+++ script_reboot_auto.sh
@@ -9,11 +9,13 @@ debit=`dump_json_keys_values "$answer" | grep down.rate | awk {'print $3'}`
 answer=$(call_freebox_api '/lan/browser/pub/ether-00:1d:7d:xx:xx:xx/')
 PC_is_up=`dump_json_keys_values "$answer" | grep result.reachable | awk {'print $3'}`
 if [ $snr -gt 50 ] && [ $debit -lt 1300 ]  && [ !PC_is_up ]; then
-	echo "`date +"%m-%d-%Y %H:%M"` - SNR_10 : $snr - Debit : $debit - PC_is_UP : $PC_is_up" >> /tmp/script.log
-	echo "`date +"%m-%d-%Y %H:%M"` - Reboot de la boiboite!">>/tmp/script.log	
-	reboot_freebox
+	echo "`date +"%m-%d-%Y %H:%M"` - SNR_10 : $snr Debit : $debit PC_is_UP : $PC_is_up" >> /tmp/script.log
+        echo "`date +"%m-%d-%Y %H:%M"` - Reboot de la boiboite!" >> /tmp/script.log
+        sleep 10
+        reboot_freebox
 else
-	echo "`date +"%m-%d-%Y %H:%M"` - SNR_10 : $snr - Debit : $debit - PC_is_UP : $PC_is_up" >> /tmp/script.log
-        echo "`date +"%m-%d-%Y %H:%M"` - RAF">>/tmp/script.log
+        echo "`date +"%m-%d-%Y %H:%M"` - SNR_10 : $snr Débit : $debit PC_is_UP : $PC_is_up" >> /tmp/script.log
+        echo "`date +"%m-%d-%Y %H:%M"` - RAF Gros" >> /tmp/script.log
+
 fi
 
